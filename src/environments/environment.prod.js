1f4cc2536f4d7dd4a9d748c0395ce79968bc6047
Mostafa Zaki <mostafazke@gmail.com>
2020-03-16 13:51:22 +0200

    add production server URL

--- src/environments/environment.prod.js
+++ src/environments/environment.prod.js
@@ -1,1 +1,1 @@
-export const environment = { api: "http://localhost:5000/" };
+export const environment = { api: "https://doodle-from-google.herokuapp.com/" };
--- src/services/productService.js
+++ src/services/productService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { environment } from "../environments/environment";
+import { environment } from "../environments/environment.prod";
 let token = localStorage.getItem("token");
 
 export default {
